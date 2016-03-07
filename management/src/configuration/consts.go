7f1b8b3fec920e850ec9853b6dfb8e18c1f6438a
Madhav Puri <madhav.puri@gmail.com>
2016-03-07 05:38:07 -0800

    set the default valid JSON to be empty
    this is needed as otherwise the default value was implicitly overriding
    the value supplied by global config.
    
    Signed-off-by: Madhav Puri <madhav.puri@gmail.com>

--- management/src/configuration/consts.go
+++ management/src/configuration/consts.go
@@ -2,5 +2,5 @@ package configuration
 
 const (
 	// DefaultValidJSON is the default JSON used when extra vars is received as empty string
-	DefaultValidJSON = `{"env": {}}`
+	DefaultValidJSON = `{}`
 )
